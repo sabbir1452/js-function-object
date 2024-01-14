9df76a50e3c05deeb5825febef29c048d675a985
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-14 18:26:19 +0600

    kfjaldf

---
+++ object.js
@@ -0,0 +1,22 @@
+var student = {
+  id: 115,
+  name: "Solaiman Khan",
+  class: 9,
+  marks: 98,
+};
+
+var mobile = {
+  brand: "Samsung",
+  price: 19000,
+  storage: "64",
+  camera: "7MP",
+};
+
+var myComputer = {
+  brand: "lenovo",
+  price: 39000,
+  color: "silver",
+  processor: "i7",
+};
+
+console.log(myComputer.price);
